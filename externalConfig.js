5ba35990e3877b0cbdfaf84ed3482561c536ef5e
cy19734682 <657683025@qq.com>
2024-08-13 03:08:53 +0000

    Deploying to gh-pages from @ cy19734682/element-design-plus@5297ef43300f0a6a9db7134670feecf07c7ddc0f 🚀

---
+++ externalConfig.js
@@ -0,0 +1,7 @@
+window._global = {
+  withCredentials: true, //是否支持跨域获取cookie
+  timeout: 30000, //接口超时时间
+  server: "", //接入层接口调用地址
+  serverImg: "", //图片拼接地址
+  bdAk: 'PG0GBB8d7buomPxGg5pDXXDZuwABtPGt' //百度地址ak
+}
